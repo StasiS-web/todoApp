fa745861e3d87bd8260d88f804ebc1073f43db36
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2025-04-09 08:57:10 +0100

    Add Forms

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
+import firebase from "firebase/compat/app";
 
 export default function Forms() {
-    const [taskName, setTaskName]=useState("")
-    const createTodo=()=>{};
-    const handleChange=(e)=>{};
+    const [taskName, setTaskName]=useState("");
+
+    const createTodo=(e)=>{
+        e.preventDefault();
+        const todoRef=firebase.database().ref("Todo");
+        const todo ={
+            taskName,
+            completed: false,
+        }
+        todoRef.push(todo);
+    };
+    const handleChange=(e)=>{
+        setTaskName(e.target.value);
+    };
 
     return (
         <form onSubmit={createTodo}>
